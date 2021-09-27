1e168cc0b20cab0e5f1aa1cc92deba699abd5325
jauhar-wibisono <13519160@std.stei.itb.ac.id>
2021-09-27 22:50:25 +0700

    refactor audio stego

--- stego-audio.py
+++ stego-audio.py
@@ -1,60 +1,81 @@
 import os
 import random
 
-def get_payload_size(audio_bytes):
+def bytes_to_arrint(_bytes):
+	return [x for x in _bytes]
+
+def arrint_to_bytes(arr_int):
+	return bytes(arr_int)
+
+def get_audio_payload_size(audio_bytes):
 	# mengembalikan ukuran pesan maksimum yang dapat di-embed file audio
 	# ukuran file dalam bit
 	return max(0, (len(audio_bytes) - 44 - 6) // 8) # 1 byte untuk seed, 5 byte untuk eof
 
-def lsb_embed(audio_bytes, message, seed = 0):
-	# mengembalikan audio (array of int) hasil penyisipan pesan
+def audio_embed(audio_bytes, message, seed = 0):
+	# mengembalikan audio (bytes) hasil penyisipan pesan
 	# KAMUS
-	# audio_bytes: array of int [0..256]
-	# message: array of int [0..256]
+	# audio_bytes: bytes OR array of int [0..256]
+	# message: bytes OR array of int [0..256]
 
-	if get_payload_size(audio_bytes) < len(message)*8:
+	# usage example:
+	# audio_steganography = AudioSteganography()
+	# result = audio_steganography.embed(audio_bytes, message_bytes)
+	# with ("result.wav", "wb") as file:
+	# 	file.write(bytes(result))
+
+	# convert params to array of int
+	if type(audio_bytes) == bytes:
+		audio_bytes = self.bytes_to_arrint(audio_bytes)
+	if type(message) == bytes:
+		message = bytes_to_arrint(message)
+
+	if get_audio_payload_size(audio_bytes) < len(message)*8:
 		raise ValueError("File is not big enough.")
 
-	message = [seed] + message + [35, 35, 35, 35, 35] # concat seed & eof
-	print(message)
+	# taruh seed di depan
+	for j in range(0, 8):
+		if (seed&(1<<j)) == 0:
+			audio_bytes[44 + j] &= (~1 & 0xFF) # turn off bit
+		else:
+			audio_bytes[44 + j] |= 1 # turn on bit
+
 	# embed
-	if seed == 0:
-		# sekuensial
+	message = message + [35, 35, 35, 35, 35] # concat seed & eof
+	if seed == 0: # sekuensial
 		for i in range(0, len(message)):
 			for j in range(0, 8):
 				if (message[i]&(1<<j)) == 0:
-					audio_bytes[44 + 8*i + j] &= (~1 & 0xFF) # turn off bit
+					audio_bytes[52 + 8*i + j] &= (~1 & 0xFF) # turn off bit
 				else:
-					audio_bytes[44 + 8*i + j] |= 1 # turn on bit
-	else:
-		# acak
-		# taruh seed di depan
-		for j in range(0, 8):
-			if (message[0]&(1<<j)) == 0:
-				audio_bytes[44 + j] &= (~1 & 0xFF) # turn off bit
-			else:
-				audio_bytes[44 + j] |= 1 # turn on bit
+					audio_bytes[52 + 8*i + j] |= 1 # turn on bit
+	else: # acak
 		# shuffle index
 		indices = [x for x in range(52, len(audio_bytes))]
 		random.Random(seed).shuffle(indices)
 		# taruh message
-		for i in range(1, len(message)):
+		for i in range(0, len(message)):
 			for j in range(0, 8):
 				if (message[i]&(1<<j)) == 0:
-					audio_bytes[indices[8*(i-1)+j]] &= (~1 & 0xFF) # turn off bit
+					audio_bytes[indices[8*i + j]] &= (~1 & 0xFF) # turn off bit
 				else:
-					audio_bytes[indices[8*(i-1)+j]] |= 1 # turn on bit
+					audio_bytes[indices[8*i + j]] |= 1 # turn on bit
 
-	return audio_bytes
+	return arrint_to_bytes(audio_bytes)
 
-def lsb_extract(audio_bytes):
-	# mengembalikan pesan (array of int) yang disisipkan dalam audio
+def audio_extract(audio_bytes):
+	# mengembalikan pesan (bytes) yang disisipkan dalam audio
+
+	# convert param to array of int
+	if type(audio_bytes) == bytes:
+		audio_bytes = bytes_to_arrint(audio_bytes)
+
+	# extract message
 	seed = 0
 	for i in range(44, 44+8):
 		seed |= (audio_bytes[i]&1)<<(i-44)
 	ret = []
-	if seed == 0:
-		# sekuensial
+	if seed == 0: # sekuensial
 		i = 0
 		while (len(ret) < 5) or (ret[-5:] != [35, 35, 35, 35, 35]):
 			add = 0
@@ -63,9 +84,7 @@ def lsb_extract(audio_bytes):
 					add |= (1<<j)
 			ret.append(add)
 			i += 1
-	else:
-		print(seed)
-		# acak
+	else: # acak
 		# shuffle index
 		indices = [x for x in range(52, len(audio_bytes))]
 		random.Random(seed).shuffle(indices)
@@ -73,23 +92,23 @@ def lsb_extract(audio_bytes):
 		while (len(ret) < 5) or (ret[-5:] != [35, 35, 35, 35, 35]):
 			add = 0
 			for j in range(0, 8):
-				if (audio_bytes[indices[8*i+j]]&1) == 1:
+				if (audio_bytes[indices[8*i + j]]&1) == 1:
 					add |= (1<<j)
 			ret.append(add)
 			i += 1
 	return ret[:-5]
 
-# tes = []
-# with open("./audio.wav", "rb") as file:
-# 	tes = [x for x in file.read()]
-# result = lsb_embed(tes, [10, 20, 30], 69)
+tes = []
+with open("./audio.wav", "rb") as file:
+	tes = [x for x in file.read()]
+result = audio_embed(tes, [10, 20, 69], 69)
 
-# with open("./result.wav", "wb") as file:
-# 	file.write(bytes(result))
+with open("./result.wav", "wb") as file:
+	file.write(bytes(result))
 
-# tes = []
-# with open("./result.wav", "rb") as file:
-# 	tes = [x for x in file.read()]
-# result = lsb_extract(tes)
+tes = []
+with open("./result.wav", "rb") as file:
+	tes = [x for x in file.read()]
+result = audio_extract(tes)
 
-# print(result)
+print(result)
