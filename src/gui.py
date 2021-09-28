6461f94aeecb3bee040cdb05072abea005099e75
jauhar-wibisono <13519160@std.stei.itb.ac.id>
2021-09-29 00:17:54 +0700

    nyicil gui

--- src/gui.py
+++ src/gui.py
@@ -12,6 +12,8 @@ WIN_SIZE = (800, 600)
 BTN_SIZE_1 = (40, 2)
 BTN_SIZE_2 = (20, 1)
 TEXTBOX_SIZE = (40, 2)
+MULTILINE_SIZE = (40, 8)
+INPUTTEXT_SIZE = (40, 1)
 TITLE_SIZE = "28px"
 
 '''
@@ -21,7 +23,8 @@ window_main_menu = [
 	[sg.Text("Modified RC4 & Steganography with LSB", size=TEXTBOX_SIZE, justification="center")],
 	[sg.Button("Modified RC4", size=BTN_SIZE_1)],
     [sg.Button("Image Steganography", size=BTN_SIZE_1)],
-    [sg.Button("Audio Steganography", size=BTN_SIZE_1)],
+    [sg.Button("Audio Steganography (Embed)", size=BTN_SIZE_1)],
+    [sg.Button("Audio Steganography (Extract)", size=BTN_SIZE_1)],
     [sg.Button("Quit", size=BTN_SIZE_1)]
 ]
 
@@ -30,8 +33,48 @@ window_image_steganography = [
 	[sg.Button("Back to Main Menu", size=BTN_SIZE_1)]
 ]
 
-window_audio_steganography = [
-	[sg.Text("Dummy", size=TEXTBOX_SIZE, justification="center")],
+window_audio_steganography_embed = [
+	[sg.Text("Embed Message", size=TEXTBOX_SIZE, justification="center")],
+	[
+		sg.Text("Message:"),
+		sg.Radio("Textbox", "message_option", default=True),
+		sg.Radio("File as text", "message_option"),
+		sg.Radio("File as binary", "message_option")
+	],
+	[
+		sg.Multiline(key="message", size=MULTILINE_SIZE)
+	],
+	[sg.FileBrowse(target="message_filename", button_text="Open message file")],
+	[sg.InputText(key="message_filename", size=INPUTTEXT_SIZE)],
+	[
+		sg.Column([
+			[sg.Text("Cover file:"), sg.FileBrowse(target="embed_cover_audio_filename", button_text="Open")],
+			[sg.Text("Stego key (positive integer):")],
+			[sg.Text("RC4 key (positive integer):")]
+		]),
+		sg.Column([
+			[sg.InputText(key="embed_cover_audio_filename", size=INPUTTEXT_SIZE)],
+			[sg.InputText(key="stego_key", size=INPUTTEXT_SIZE)],
+			[sg.InputText(key="embed_rc4_key", size=INPUTTEXT_SIZE)]
+		])
+	],
+	[sg.Button("Embed", size=BTN_SIZE_1)],
+	[sg.Button("Back to Main Menu", size=BTN_SIZE_1)]
+]
+
+window_audio_steganography_extract = [
+	[sg.Text("Extract Message", size=TEXTBOX_SIZE, justification="center")],
+	[
+		sg.Column([
+			[sg.Text("Cover file:"), sg.FileBrowse(target="extract_cover_audio_filename", button_text="Open")],
+			[sg.Text("RC4 key (positive integer):")]
+		]),
+		sg.Column([
+			[sg.InputText(key="extract_cover_audio_filename", size=INPUTTEXT_SIZE)],
+			[sg.InputText(key="extract_rc4_key", size=INPUTTEXT_SIZE)]
+		])
+	],
+	[sg.Button("Extract", size=BTN_SIZE_1)],
 	[sg.Button("Back to Main Menu", size=BTN_SIZE_1)]
 ]
 
@@ -39,15 +82,14 @@ all_windows = [
 	[
 		sg.Column(window_main_menu, key="window_main_menu"),
 		sg.Column(window_image_steganography, key="window_image_steganography", visible=False),
-		sg.Column(window_audio_steganography, key="window_audio_steganography", visible=False)
+		sg.Column(window_audio_steganography_embed, key="window_audio_steganography_embed", visible=False),
+		sg.Column(window_audio_steganography_extract, key="window_audio_steganography_extract", visible=False)
 	]
 ]
-
 
 '''
 Runner
 '''
-
 def run_gui():
 	window = sg.Window("Modified RC4 & Steganography with LSB", all_windows, size=WIN_SIZE, element_justification="c")
 
@@ -58,13 +100,18 @@ def run_gui():
 			window["window_main_menu"].update(visible=False)
 			window["window_image_steganography"].update(visible=True)
 
-		if "Audio Steganography" in cur_events: # kalo tombol "Audio Steganography" diteken
+		if "Audio Steganography (Embed)" in cur_events: # kalo tombol "Audio Steganography (Embed)" diteken
 			window["window_main_menu"].update(visible=False)
-			window["window_image_steganography"].update(visible=True)
+			window["window_audio_steganography_embed"].update(visible=True)
+
+		if "Audio Steganography (Extract)" in cur_events: # kalo tombol "Audio Steganography (Extract)" diteken
+			window["window_main_menu"].update(visible=False)
+			window["window_audio_steganography_extract"].update(visible=True)
 
 		if "Back to Main Menu" in cur_events: # kalo tombol "Back to Main Menu" diteken
 			window["window_image_steganography"].update(visible=False)
-			window["window_audio_steganography"].update(visible=False)
+			window["window_audio_steganography_embed"].update(visible=False)
+			window["window_audio_steganography_extract"].update(visible=False)
 			window["window_main_menu"].update(visible=True)
 
 		if cur_events == sg.WIN_CLOSED or 'Quit' in cur_events:
